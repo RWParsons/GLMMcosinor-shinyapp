4cc3baa28aad71d482764591aaac7824bc8bccbf
oliverjayasinghe <oliver.g.jayasinghe@gmail.com>
2023-12-06 14:58:57 +1000

    Update simulate_data.R

--- simulate_data.R
+++ simulate_data.R
@@ -50,11 +50,11 @@ dat_mixed_2 <- do.call(
     f_sample_id_2(
       id_num = x,
       mesor = rnorm(1, mean = 0, sd = 1),
-      amp = rnorm(1, mean = 3, sd = 0.5),
-      acro = rnorm(1, mean = 1.5, sd = 0.2),
+      amp = 10,
+      acro = pi,
       beta.mesor = rnorm(1, mean = 10, sd = 1),
-      beta.amp = rnorm(1, mean = 5, sd = 0.5),
-      beta.acro = rnorm(1, mean = 1.5, sd = 0.2),
+      beta.amp = 10,
+      beta.acro = -pi,
       period = 24,
       n_components = 1,
       sd = 0.2,
@@ -72,3 +72,43 @@ mixed_mod_2 <- cglmm(
   ) + (1 + amp_acro1 | subject),
   data = dat_mixed_2
 )
+
+
+# #simulates multiple polar plots. Work in progress
+# dataset <- NULL
+# moving_model <- NULL
+# polar_plots <- NULL
+# n = 10
+# sd = 0.5
+# beta.sd = 0.5
+# mesor = 10 
+# amp = 3
+# acro = 2
+# beta.mesor = 6
+# beta.amp = 5
+# beta.acro = 1
+# 
+# for (i in 1:4) {
+# dataset[[i]] <- simulate_cosinor(
+#   n = n,
+#   mesor = mesor,
+#   amp = amp,
+#   acro = acro + 0.5*i,
+#   family = 'gaussian',
+#   sd = sd,
+#   beta.sd = beta.sd,
+#   period = 24,
+#   n_components = 1,
+#   beta.mesor = beta.mesor,
+#   beta.amp = beta.amp,
+#   beta.acro = beta.acro,
+#   beta.group = TRUE
+# )
+# moving_model[[i]] <- cglmm(
+#   Y ~ group + amp_acro(times,
+#                        n_components = 1,
+#                        period = 24,
+#                        group = "group"
+#   ),data = dataset[[i]])
+# polar_plots[[i]] <- polar_plot(moving_model[[i]])
+# }
